9295e5303cdd1ccfa2c13f55506d84c9ffea073c
Kethavath Nandini <kethavathnandini91@gmail.com>
2026-08-08 18:53:05 +0530

    learn basic constraints and base file

---
+++ part2/01_sql_concepts_base_files.sql
@@ -0,0 +1,27 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    
+    name TEXT NOT NULL,
+    cartogory TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
+
+    stock INTEGER NOT NULL DEFAULT 0 CHECK (STOCK >= 0),
+
+    is_active BOOLEAN NOT NULL DEFAULT true,
+    sku TEXT UNIQUE,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO product(name, cartogory, price, stock, is_active, sku,description)
+VALUES
+('Wireless Mouse', 'Electronics', 3499.00, 18, TRUE, 'ELEC-004', 'Wireless computer mouse'),
+('Casual T-Shirt', 'Clothing', 1299.00, 25, TRUE, 'CLTH-002', 'Cotton casual t-shirt'),
+('Formal Shoes', 'Footwear', 4599.00, 8, TRUE, 'FTWR-002', 'Leather formal shoes'),
+('SQL Guide', 'Books', 799.00, 30, TRUE, 'BOOK-002', 'SQL programming guide'),
+('Laptop Stand', 'Accessories', 599.00, 0, FALSE, 'ACCS-002', 'Adjustable laptop stand');
+SELECT * FROM product;
---
+++ part2/02_select_specific_cols.sql
@@ -0,0 +1,8 @@
+-- select * - returns every column
+-- select specific column gives that i want
+
+-- SELECT * FROM product;
+
+SELECT name, cartogory, price from product;
+
+--alias (AS) 
